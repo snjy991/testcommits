de83eb0c68d65b852b4d18049b2b8697a1a9930f
sanjay chaudhary <snjy991@gmail.com>
2020-03-10 14:47:32 +0530

    Update ProjectList.groovy

--- ProjectList.groovy
+++ ProjectList.groovy
@@ -1,14 +1,4 @@
 def ProjectList() {
-import groovy.json.JsonSlurper
-def json_str = '''{
-   "name": "Foo Bar",
-   "year": 2018,
-   "timestamp": "2018-03-08T00:00:00",
-   "tags": [ "person", "employee" ],
-   "grade": 3.14 }'''
- 
- 
-def jsonSlurper = new JsonSlurper()
-plist = jsonSlurper.parseText(json_str)
-return plist
+def list =["test1","test2","test3"]
+  return list
 }
